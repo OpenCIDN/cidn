2f93754defe332a64d5394c4369c7ec22094db31
Shiming Zhang <wzshiming@hotmail.com>
2025-11-25 16:18:21 +0800

    Downgrade k8s

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -22,7 +22,7 @@ DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 ROOT_DIR="$(realpath "${DIR}/..")"
 
-KUBE_VERSION=v0.34.1
+KUBE_VERSION=v0.33.6
 
 function deepcopy-gen() {
   go run k8s.io/code-generator/cmd/deepcopy-gen@${KUBE_VERSION} "$@"
