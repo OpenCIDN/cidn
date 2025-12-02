ded5ca68ce78b193e7a8f982b901726673b3e773
Shiming Zhang <wzshiming@hotmail.com>
2025-12-02 18:45:09 +0800

    Update chunk progress status after processing global result values

--- pkg/runner/chunk_runner.go
+++ pkg/runner/chunk_runner.go
@@ -674,6 +674,12 @@ func (r *ChunkRunner) process(continues <-chan struct{}, chunk *v1alpha1.Chunk) 
 		return
 	}
 
+	s.Update(func(ss *v1alpha1.Chunk) *v1alpha1.Chunk {
+		if *gsr != nil {
+			updateProgress(&ss.Status, &ss.Spec, *gsr, *gdrs)
+		}
+	})
+
 	chunk = s.Get()
 	if chunk.Spec.Total > 0 && chunk.Status.Progress != chunk.Spec.Total {
 		r.unmarkRecord(chunk.Name)
